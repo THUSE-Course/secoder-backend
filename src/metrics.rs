5a3e430bcd3e5c6ca056ae12f7e5bdfbed98288c
Xuelin Yang <xuelin@adamanteye.cc>
2026-02-12 15:34:52 +0800

    feat: add more metrics

--- src/metrics.rs
+++ src/metrics.rs
@@ -14,25 +14,33 @@ pub async fn render_metrics(
     let users_total = user::count_users(db).await?;
     let users_ungrouped_total = user::count_ungrouped_users(db).await?;
     let groups_total = group::count_groups(db).await?;
+    let (least_group, most_group) = group::least_and_most_members(db).await?;
     let invitations_pending_total =
         invite::count_pending_invitations(db).await?;
-
     Ok(format!(
-        "# HELP secoder_users_total Total number of users.\n\
-# TYPE secoder_users_total gauge\n\
-secoder_users_total {users}\n\
-# HELP secoder_users_ungrouped_total Total number of users not in a group.\n\
-# TYPE secoder_users_ungrouped_total gauge\n\
-secoder_users_ungrouped_total {ungrouped}\n\
-# HELP secoder_groups_total Total number of groups.\n\
-# TYPE secoder_groups_total gauge\n\
-secoder_groups_total {groups}\n\
-# HELP secoder_invitations_pending_total Total number of pending invitations.\n\
-# TYPE secoder_invitations_pending_total gauge\n\
-secoder_invitations_pending_total {pending}\n",
+        "# HELP secoder_users Total number of users.\n\
+# TYPE secoder_users gauge\n\
+secoder_users {users}\n\
+# HELP secoder_users_ungrouped Total number of users not in a group.\n\
+# TYPE secoder_users_ungrouped gauge\n\
+secoder_users_ungrouped {ungrouped}\n\
+# HELP secoder_groups Total number of groups.\n\
+# TYPE secoder_groups gauge\n\
+secoder_groups {groups}\n\
+# HELP secoder_group_members_least Number of members in the least man-powered group.\n\
+# TYPE secoder_group_members_least gauge\n\
+secoder_group_members_least {least_group}\n\
+# HELP secoder_group_members_most Number of members in the most man-powered group.\n\
+# TYPE secoder_group_members_most gauge\n\
+secoder_group_members_most {most_group}\n\
+# HELP secoder_invitations_pending Total number of pending invitations.\n\
+# TYPE secoder_invitations_pending gauge\n\
+secoder_invitations_pending {pending}\n",
         users = users_total,
         ungrouped = users_ungrouped_total,
         groups = groups_total,
+        least_group = least_group,
+        most_group = most_group,
         pending = invitations_pending_total
     ))
 }
--- src/metrics/group.rs
+++ src/metrics/group.rs
@@ -1,7 +1,33 @@
 use super::*;
+use sea_orm::QueryOrder;
 
-use crate::entity::group;
+use crate::entity::{group, member};
 
 pub async fn count_groups(db: &DatabaseConnection) -> Result<u64, AppError> {
     Ok(group::Entity::find().count(db).await?)
+}
+
+pub async fn least_and_most_members(
+    db: &DatabaseConnection,
+) -> Result<(u8, u8), AppError> {
+    let groups = group::Entity::find()
+        .order_by_asc(group::Column::CodeName)
+        .all(db)
+        .await?;
+    let mut least: Option<u8> = None;
+    let mut most: Option<u8> = None;
+
+    for g in groups {
+        let current = member::Entity::find()
+            .filter(member::Column::GroupCodeName.eq(&g.code_name))
+            .count(db)
+            .await? as u8;
+        if least.as_ref().is_none_or(|v| current < *v) {
+            least = Some(current);
+        }
+        if most.as_ref().is_none_or(|v| current > *v) {
+            most = Some(current);
+        }
+    }
+    Ok((least.unwrap_or_default(), most.unwrap_or_default()))
 }
